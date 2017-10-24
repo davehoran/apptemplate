029d6511dec6d93b9c97b3c501ad9059fa748611
davehoran <dave.horan@gmail.com>
2017-10-24 16:48:53 -0400

    checkin mid-work on homepage auth check.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-home',
@@ -10,12 +11,29 @@ export class HomePage {
 
   constructor(
     public navCtrl: NavController,
-    private afAuth: AngularFireAuth) {
+    public navParams: NavParams,
+    private afAuth: AngularFireAuth,
+    private toast: ToastController) {
 
   }
 
-  ionViewWillLoad() {
-    this.afAuth.authState.subscribe(data => console.log(data));
+  ionViewWillEnter() {
+    this.afAuth.authState.subscribe(data => {
+      if(data.email && data.uid){
+      this.toast.create({
+        message: 'Welcome to the skeleton app. This is a template for simple user authenticated applciation.',
+        duration: 5000,
+        showCloseButton: true
+      }).present();
+    } else {
+      this.toast.create({
+        message: 'User not logged in...',
+        duration: 5000
+      }).present();
+      this.navCtrl.setRoot(LoginPage);
+    }
+    });
+
   }
 
 }
